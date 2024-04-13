8c9d3d1da9386959fc07eb77d4fac94ccf3b5212
박수빈 <subin57419@gmail.com>
2024-04-13 17:00:14 +0900

    toggle 사용하기

--- app.js
+++ app.js
@@ -228,7 +228,7 @@
 // window.addEventListener("online", handleWindowOnline);
 
 
-const h1 = document.querySelector(".hello h1");
+// const h1 = document.querySelector(".hello h1");
 
 // function handleTitleClick() {
 //     console.log(h1.style.color);
@@ -246,16 +246,43 @@ const h1 = document.querySelector(".hello h1");
 // }
 
 
-function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "tomato";
-    } else {
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
-}
+// function handleTitleClick() {
+//     const currentColor = h1.style.color;
+//     let newColor;
+//     if (currentColor === "blue") {
+//         newColor = "tomato";
+//     } else {
+//         newColor = "blue";
+//     }
+//     h1.style.color = newColor;
+// }
 
+
+// h1.addEventListener("click", handleTitleClick);
+
+
+// const h1 = document.querySelector(".hello h1");
+
+// function handleTitleClick() {
+//     const clickedClass = "clicked";
+//     if (h1.classList.contains(clickedClass)) {
+//         h1.classList.remove(clickedClass);
+//     } else {
+//         h1.classList.add(clickedClass);
+//     }
+// }
+
+// h1.addEventListener("click", handleTitleClick);
+
+
+const h1 = document.querySelector(".hello h1");
+
+function handleTitleClick() {
+    // toggle은 h1의 classList에 clicked class가 이미 있는지 확인해서
+    // 만약 있다면, toggle이 clicked를 제거한다
+    // 만약 h1의 classlist에 clicked class가 없다면
+    // clicked를 classList에 추가한다
+    h1.classList.toggle("clicked");
+}
 
 h1.addEventListener("click", handleTitleClick);

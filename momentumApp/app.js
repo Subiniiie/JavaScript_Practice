f236a8f8ee9ac5acc0134cd092bab88a8860ac0c
박수빈 <subin57419@gmail.com>
2024-04-16 17:56:18 +0900

    username 구현 중

--- momentumApp/app.js
+++ momentumApp/app.js
@@ -29,10 +29,11 @@ function onLoginSubmit(event) {
     localStorage.setItem(USERNAME_KEY, username);
     // 같은 값 출력
     // greeting.innerText = "Hello " + username;
-    paintGreetings(username);
+    paintGreetings();
 }
 
-function paintGreetings(username) {
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);    
 }
@@ -41,6 +42,6 @@ if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreetings(savedUsername);
+    paintGreetings();
     loginForm.classList.add(HIDDEN_CLASSNAME);
 }

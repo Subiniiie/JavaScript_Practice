c470602bc99ae1ff3aa5df2fbc1f4165c1708496
박수빈 <subin57419@gmail.com>
2024-04-19 23:37:03 +0900

    localStorage 문자열 저장

--- momentumApp/js/todo.js
+++ momentumApp/js/todo.js
@@ -2,6 +2,13 @@ const toDoForm = document.getElementById("todo-form")
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.getElementById("todo-list")
 
+
+const toDos = []
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos))
+}
+
 function deleteTodo(event) {
     const li = event.target.parentElement
     li.remove()
@@ -24,7 +31,9 @@ function handleToDoSubmit(event) {
     event.preventDefault()
     const newTodo = toDoInput.value
     toDoInput.value = ""
+    toDos.push(newTodo)
     paintTodo(newTodo)
+    saveToDos()
 }
 
 
